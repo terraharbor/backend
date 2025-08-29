c9de3a484a6c43f1cac81cb80eea728de59f52f9
fabricechapuis <fabrice.chapuis@heig-vd.ch>
2025-08-29 14:30:51 +0200

    refactor: Implemented password salting

--- user.py
+++ user.py
@@ -5,5 +5,6 @@ class User(BaseModel):
     username: str
     disabled: Optional[bool] = None
     sha512_hash: str
+    salt: str
     token: Optional[str] = None
     token_validity: Optional[int] = None
