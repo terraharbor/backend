c97cef0eb93acad787da6649f23b5c99b6b1a2bc
fabricechapuis <fabrice.chapuis@heig-vd.ch>
2025-09-02 15:14:08 +0200

    feat: health check endpoint added

--- main.py
+++ main.py
@@ -37,6 +37,12 @@ def _latest_state_path(project: str, state_name: str) -> str:
 def _versioned_state_path(project: str, state_name: str, version: int) -> str:
     return os.path.join(_state_dir(project, state_name), f"{version}.tfstate")
 
+@app.get("/health_check")
+async def health_check() -> dict:
+    """
+    Health check endpoint.
+    """
+    return {"status": "ok"}
 
 @app.post("/register")
 async def register(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Response:
