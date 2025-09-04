e4b98026567f0e60f13732f0361fcda15dba9129
Svelva <sven.ferreirasilva@heig-vd.ch>
2025-09-04 18:04:34 +0200

    feat: added teams management methods

---
+++ teams.py
@@ -0,0 +1,86 @@
+import logging
+from secrets import token_hex
+
+from auth_functions import get_db_connection, get_user_id
+from team_accesses import fetch_team_token_for_username_and_team
+
+logger = logging.getLogger(__name__)
+
+def create_team(creator_name: str, team_name: str, desc: str) -> str:
+    conn = get_db_connection()
+    with conn:
+        with conn.cursor() as cur:
+            # Create org
+            cur.execute("""
+                        INSERT INTO teams (name, description) VALUES (%s, %s)
+                        """, (team_name, desc))
+
+            # Create token, creator is admin
+            new_tok = token_hex(32)
+
+            cur.execute("""
+            SELECT id FROM teams WHERE name = %s""", (team_name,))
+
+            row = cur.fetchone()
+            team_id = row[0]
+            user_id = get_user_id(creator_name)
+
+            # Create token
+            cur.execute("""
+            INSERT INTO team_tokens (token, teamId, userId, administrator, can_add_proj, can_del_proj, can_add_token, can_del_token)
+                        VALUES (%s, %s, %s, B'%s', B'%s', B'%s', B'%s', B'%s')""", (new_tok, team_id, user_id, 1, 0, 0, 0, 0))
+
+            return new_tok
+
+
+def delete_team(deleter_name: str, team_id: str) -> None:
+    # Check for permissions
+    deleter_perms = fetch_team_token_for_username_and_team(deleter_name, team_id)
+
+    if deleter_perms and deleter_perms.admin:
+        conn = get_db_connection()
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                            DELETE FROM teams WHERE id = %s""", (team_id,))
+    else:
+        logger.error("You are not allowed to delete team.")
+
+
+def get_teams_for_user(username: str) -> list[dict]:
+    conn = get_db_connection()
+
+    user_id = get_user_id(username)
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+            SELECT o.id, o.name, o.description
+            FROM teams o
+            JOIN team_tokens ot ON o.id = ot.teamId
+            WHERE ot.userId = %s
+            """, (user_id,))
+
+            rows = cur.fetchall()
+            if rows:
+                out = []
+                for row in rows:
+                    team_id, name, desc = row
+                    out.append({"id": team_id, "name": name, "description": desc})
+
+                return out
+            else:
+                logger.error(f"Error when fetching teams for user {username}")
+                return [{"Error": "notFound"}]
+
+
+def update_team_by_id(updater_name: str, team_id: str, contents: dict) -> None:
+    updater_perms = fetch_team_token_for_username_and_team(updater_name, team_id)
+
+    if updater_perms and updater_perms.admin:
+        conn = get_db_connection()
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                UPDATE teams
+                SET name = %s, description = %s
+                WHERE id = %s""", (contents['name'], contents['description'], team_id))
